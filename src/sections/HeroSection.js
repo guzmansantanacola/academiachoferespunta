6a3e3f6217ffeac81f5dbb7af93d50dbe5b72fb7
guzmansantanacola <guzmansantanacola@gmail.com>
2025-12-21 22:14:33 -0300

    titles fixes

--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -6,13 +6,13 @@ function HeroSection() {
     <section id="inicio" className="hero-section">
       <Container>
         <Row className="align-items-center">
-          <Col lg={7}>
+          <Col lg={7} className="text-center text-lg-start">
             <h1>Aprende a Conducir con Confianza</h1>
             <p>
               Tu camino hacia la libertad comienza aquí. Instructores certificados,
               vehículos modernos y horarios flexibles para tu comodidad.
             </p>
-            <Button className="btn-primary-custom" href="/contacto">
+            <Button className="btn-primary-custom mx-auto mx-lg-0 mb-2 d-block d-lg-inline-block" href="/contacto">
               ¡Inscríbete Ahora!
             </Button>
           </Col>
